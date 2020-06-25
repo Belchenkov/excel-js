a8ad37e86c8ea380d678137d51e4d1060d0b62e6
Aleksey Belchenkov <u608110@gmail.com>
2020-06-25 14:08:21 +0300

    Setting Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,40 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: "index.html"
         }),
-        new CopyPlugin([
+        /*new CopyPlugin([
             {
                 from: path.resolve(__dirname, 'src/favicon.ico'),
                 to: path.resolve(__dirname, 'dist')
             }
-        ]),
+        ]),*/
         new MiniCssExtractPlugin({
             filename: 'bundle.[hash].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        },
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ],
+    }
 };
