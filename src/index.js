0fce5c6a467d023c218776318392536b196e5797
Aleksey Belchenkov <u608110@gmail.com>
2020-06-28 14:02:25 +0300

    Added isDev & isPord

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './module';
 import './scss/index.scss';
 
-console.log('Work')
+console.log('Works')
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,20 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: "development",
-    entry: './index.js',
+    entry: [
+        '@babel/polyfill',
+        './index.js'
+    ],
     output: {
-        filename: 'bundle.[hash].js',
+        filename: filName('js'),
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
@@ -19,19 +27,31 @@ module.exports = {
             '@core': path.resolve(__dirname, 'src/core')
         }
     },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
-            template: "index.html"
-        }),
-        /*new CopyPlugin([
-            {
-                from: path.resolve(__dirname, 'src/favicon.ico'),
-                to: path.resolve(__dirname, 'dist')
+            template: "index.html",
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
             }
-        ]),*/
+        }),
         new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css'
+            filename: filName('css')
+        }),
+        new CopyPlugin({
+            patterns:
+            [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
         })
     ],
     module: {
