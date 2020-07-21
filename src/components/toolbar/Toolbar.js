a375d5d3dd76ad3107e36c48229ccc3011b75dd3
Aleksey Belchenkov <u608110@gmail.com>
2020-07-21 13:30:40 +0300

    Output Template in Components

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,7 +1,28 @@
 import {ExcelComponent} from "@core/ExcelComponent";
 
 export class Toolbar extends ExcelComponent {
+    static className = 'excel__toolbar';
+
     toHTML() {
-        return '<h1>Toolbar</h1>'
+        return `
+            <div class="button">
+                <div class="material-icons">format_align_left</div>
+            </div>
+            <div class="button">
+                <div class="material-icons">format_align_center</div>
+            </div>
+            <div class="button">
+                <div class="material-icons">format_align_right</div>
+            </div>
+            <div class="button">
+                <div class="material-icons">format_bold</div>
+            </div>
+            <div class="button">
+                <div class="material-icons">format_italic</div>
+            </div>
+            <div class="button">
+                <div class="material-icons">format_underlined</div>
+            </div>
+        `;
     }
 }
